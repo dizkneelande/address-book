805c4dc88fbb66298ba76ec74657b5f959c678dd
dizkneelande <nell.yi@outlook.com>
2024-03-16 16:20:10 +1000

    name getter now includes email

--- src/main/java/com/example/addressbook/MainController.java
+++ src/main/java/com/example/addressbook/MainController.java
@@ -1,4 +1,56 @@
 package com.example.addressbook;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+
 public class MainController {
+    @FXML
+    private ListView<Contact> contactsListView;
+    private IContactDAO contactDAO;
+    public MainController() {
+        contactDAO = new MockContactDAO();
+        contactDAO.addContact(new Contact("Jerry", "Doe", "jerrydoe@example.com", "0423423426"));
+    }
+
+    /**
+     * Renders a cell in the contacts list view by setting the text to the contact's full name.
+     * @param contactListView The list view to render the cell for.
+     * @return The rendered cell.
+     */
+    private ListCell<Contact> renderCell(ListView<Contact> contactListView) {
+        return new ListCell<>() {
+            /**
+             * Updates the item in the cell by setting the text to the contact's full name.
+             * @param contact The contact to update the cell with.
+             * @param empty Whether the cell is empty.
+             */
+            @Override
+            protected void updateItem(Contact contact, boolean empty) {
+                super.updateItem(contact, empty);
+                // If the cell is empty, set the text to null, otherwise set it to the contact's full name
+                if (empty || contact == null || contact.getContactSummary() == null) {
+                    setText(null);
+                } else {
+                    setText(contact.getContactSummary());
+                }
+            }
+        };
+    }
+
+    /**
+     * Synchronizes the contacts list view with the contacts in the database.
+     */
+    private void syncContacts() {
+        contactsListView.getItems().clear();
+        contactsListView.getItems().addAll(contactDAO.getAllContacts());
+    }
+
+    @FXML
+    public void initialize() {
+        contactsListView.setCellFactory(this::renderCell);
+        syncContacts();
+    }
+
+
 }
